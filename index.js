df3c8084c798e08713e93d6895467fdd5dc3620e
GarbageCode1984 <rkqlwl1984@gmail.com>
2022-11-02 19:48:38 +0900

    mongDB

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const mongoose = require("mongoose");
+mongoose
+    .connect(
+        "mongodb+srv://rkqlwl1984:djqlf0595!@cluster0.kmasd50.mongodb.net/test",
+        {
+            useNewUrlParser: true,
+        }
+    )
+    .then(() => console.log("MongoDB Connected..."))
+    .catch((err) => console.log(err));
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
