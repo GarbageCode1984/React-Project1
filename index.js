c00198fa1a954cc8e53db7c004476df1320e302e
GarbageCode1984 <rkqlwl1984@gmail.com>
2022-11-20 22:12:06 +0900

    a

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const { User } = require("./models/User");
+const { auth } = require("./middleware/auth");
 const config = require("./config/key");
 const mongoose = require("mongoose");
 
@@ -17,7 +18,7 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
     // 회원 가입 할때 필요한 정보들을 client에서 가져오면 그것들을 DB에 넣어준다.
     const user = new User(req.body);
 
@@ -27,7 +28,7 @@ app.post("/register", (req, res) => {
     });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
     //요청된 이메일을 DB에서 찾는다.
     User.findOne({ email: req.body.email }, (err, user) => {
         if (!user) {
@@ -53,6 +54,21 @@ app.post("/login", (req, res) => {
                     .json({ loginSuccess: true, userId: user._id });
             });
         });
+    });
+});
+
+//role 1 어드민
+//role 0 일반유저, role 0이 아니면 관리자
+app.get("/api/users/auth", auth, (req, res) => {
+    //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 뜻
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        role: req.user.role,
+        image: req.user.image,
     });
 });
 
